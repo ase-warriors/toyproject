5b5f63a0bf5a4422c873199dc6772ce824a59bfe
Andy Lianghua Xu <lx2180@columbia.edu>
2017-10-03 22:36:01 -0400

    add create db scripts

---
+++ install_db.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+while true; do
+    read -p "Mac: Install PostgreSQL? (please install if you don't have it yet)" yn
+    case $yn in
+        [Yy]* ) brew install postgresql; break;;
+        [Nn]* ) break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+while true; do
+    read -p "Ubuntu/Debian Users: Install PostgreSQL? (please install if you don't have it yet)" yn
+    case $yn in
+        [Yy]* ) sudo apt install postgres; break;;
+        [Nn]* ) break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+while true; do
+    read -p "Create a new database? (please do so if you haven't done so)" yn
+    case $yn in
+        [Yy]* ) createdb warriors; break;;
+        [Nn]* ) break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+while true; do
+    read -p "Create a new table? (please do so if you haven't done so)" yn
+    case $yn in
+        [Yy]* ) psql -f create_tables.txt ; break;;
+        [Nn]* ) break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
