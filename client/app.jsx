330b79fdbac7e72f0da2ee32dcf32594cd09e88b
Andy Lianghua Xu <lx2180@columbia.edu>
2017-10-03 22:14:07 -0400

    complete the toy project

--- client/app.jsx
+++ client/app.jsx
@@ -1,6 +1,7 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
 const Button = require('react-bootstrap').Button;
+const d3 = require('d3');
 
 console.log('this is being called');
 
@@ -12,12 +13,20 @@ class Home extends React.Component {
         };
         this.addEntry = this.addEntry.bind(this);
     }
+    componentDidMount() {
+        d3.json('/get', (err, json) => {
+            console.log(`we got ${JSON.stringify(json)}`);
+            this.setState({entries: json.data});
+        });
+    }
     addEntry() {
         console.log('database added');
         const current_entries = this.state.entries;
         this.setState({entries: current_entries+1});
         // report an entry addition using POST
-        
+        d3.request('/update').post("hello", (err, json) => {
+            console.log('we updated!');
+        })
     }
     render() {
         return (
--- server/server.js
+++ server/server.js
@@ -1,11 +1,32 @@
 const express = require('express');
 const app = express()
+const { Client } = require('pg')
 
+const dbclient = new Client()
+
+dbclient.connect()
 app.get('/', (req, res) => {
   res.send(index_page);
 });
 
-app.use('/public', express.static('../client/public/'));
+app.get('/get', (req2, res2) => {
+  dbclient.query('SELECT entry_count FROM Vendor', (err, res) => {
+    console.log(err ? err.stack : res.rows);
+    const jsonreply = {
+      data: res.rows.length
+    };
+    res2.json(jsonreply);
+  });
+});
+
+app.post('/update', (req2, res2) => {
+  dbclient.query('INSERT INTO Vendor VALUES (12)', (err, res) => {
+    console.log(err ? err.stack : res.rows);
+    res2.end();
+  });
+});
+
+app.use('/public', express.static('client/public/'));
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
